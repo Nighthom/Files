c3cbb777e7b75e13a39710560f28ab2a4e445a5c
Nighthom <48198262+Nighthom@users.noreply.github.com>
2022-05-03 18:01:58 +0900

    Update Hello World.c

--- Study/C/src/Hello World.c
+++ Study/C/src/Hello World.c
@@ -10,10 +10,10 @@ disasm (x86-64)
   push rdi
   sub rsp, 0e8h
   lea rbp, [rsp+20h]
-  lea rcx, [__9CE99571_main@c (07FF7C40F1008h)]
-  call __CheckForDebuggerJustMyCode (07FF7C40E135Ch)
-  lea rcx, [string "Hello World!\n" (07FF7C40E9C28h)]
-  call printf (07FF7C40E118Bh)
+  lea rcx, [__9CE99571_main@c]
+  call __CheckForDebuggerJustMyCode
+  lea rcx, [string "Hello World!\n"]
+  call printf
   xor eax, eax
   lea rsp,[rbp+0C8h]  
   pop rdi  
